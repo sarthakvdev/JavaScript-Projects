64b909459f0b14788f024a4a97919295b7561b9f
Sarthak Verma <sarthakvdev@gmail.com>
2020-09-28 14:57:27 +0530

    Forkify other files added

---
+++ Forkify/src/js/index.js
@@ -0,0 +1,5 @@
+// Global app controller
+import num from './test';
+
+const x = 23;
+console.log(`I imported ${num} from another module called test.js and variable x is ${x}`);
---
+++ Forkify/src/js/test.js
@@ -0,0 +1,2 @@
+console.log("Modules imported");
+export default 123456;
